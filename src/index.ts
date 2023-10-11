55601cd6f6123dd98e1a6b5c19bdc5f7339f3ae0
Gabriel Rieff <gabrielrieff1@gmail.com>
2023-10-10 23:16:14 -0300

    feat: add prisma

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,19 @@ import { config } from "dotenv";
 import { GetUsersController } from "./Controllers/get-users/get-users";
 import { PostgresGetUsers } from "./repositories/postgress-get-user";
 
-config();
-const app = express();
-const port = process.env.PORT || 3000;
+const main = async () => {
+  config();
+  const app = express();
 
-app.get("/users", async (req, res) => {
-  const postgresUserRepositoriy = new PostgresGetUsers();
-  const getUsersController = new GetUsersController(postgresUserRepositoriy);
-  const { body, statusCode } = await getUsersController.handle();
-  res.send(body).status(statusCode).end();
-});
+  app.get("/users", async (req, res) => {
+    const postgresUserRepositoriy = new PostgresGetUsers();
+    const getUsersController = new GetUsersController(postgresUserRepositoriy);
+    const { body, statusCode } = await getUsersController.handle();
+    res.send(body).status(statusCode).end();
+  });
 
-app.listen(port, () => console.log(`listening on port ${port}!`));
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => console.log(`listening on port ${port}!`));
+};
+
+main();
--- src/repositories/postgress-get-user.ts
+++ src/repositories/postgress-get-user.ts
@@ -1,8 +1,12 @@
 import { IGetUsersRepository } from "../Controllers/get-users/protocols";
+import client from "../database/postgres";
 import { User } from "../models/user";
 
 export class PostgresGetUsers implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
+
+    const users = await client.
+
     return [
       {
         firtsName: "John",
