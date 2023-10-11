e6163b9e4fe7a7b9063d82a8cc6d824729b76462
Gabriel Rieff <gabrielrieff1@gmail.com>
2023-10-10 21:54:51 -0300

    feat: add express intial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello world!");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
