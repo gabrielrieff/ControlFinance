54f3df0ed715a1237bca892b6159029d3dc0123d
Gabriel Rieff <gabrielrieff1@gmail.com>
2023-10-26 11:02:32 -0300

    feat: primary config

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,20 +1,31 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    colors: {
+      grey: {
+        300: "#E8E8E8",
+        400: "#AAAAAA",
+        700: "#292D32",
+      },
+      green: {
+        200: "#ABFFC3",
+        500: "#03A200",
+      },
+      blue: {
+        200: "#D1ECFF",
+        500: "#0500FF",
+      },
+      red: {
+        200: "#FFA4A4",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
