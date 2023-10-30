7bf5d470efe5cf3a50ef4b139ccb29e5169b2870
Gabriel Rieff <gabrielrieff1@gmail.com>
2023-10-30 14:43:48 -0300

    refactor: routers

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,37 +1,12 @@
 import { Router } from "express";
-
-import { isAuthenticated } from "./middlewares/isAuthenticated";
-
-import { CreateUserController } from "./Controllers/User/create-user/create-user";
-import { UpdateUserController } from "./Controllers/User/update-user/update-user";
-import { deleteUserController } from "./Controllers/User/delete-user/delete-user";
-import { AuthUserController } from "./Controllers/User/auth-user/auth-user";
-import { GetUsersController } from "./Controllers/User/get-users/get-users";
-import { ToForgotPasswordController } from "./Controllers/User/to-forgot-password/to-forgot-password";
-import { ToRecoverPasswordController } from "./Controllers/User/to-recover-user/to-recover-password";
-import { ResetPasswordController } from "./Controllers/User/reset-password/reset-password";
-import { CreateInovoiceController } from "./Controllers/Inovoice/create-inovoice/create-inovoice";
+import { invoiceRouter } from "./routes/invoiceRoute";
+import { userRouter } from "./routes/userRoutes";
+import { categoryRouter } from "./routes/categoryRouter";
 
 const router = Router();
 
-//User
-router.post("/session", new AuthUserController().handle);
-router.post("/user", isAuthenticated, new CreateUserController().handle);
-router.get("/users", isAuthenticated, new GetUsersController().handle);
-router.delete("/user/:id", isAuthenticated, new deleteUserController().handle);
-router.patch("/user/:id", isAuthenticated, new UpdateUserController().handle);
-router.post("/forgotpassword", new ToForgotPasswordController().handle);
-router.post("/recoverpassword", new ToRecoverPasswordController().handle);
-router.post(
-  "/resetpassword",
-  isAuthenticated,
-  new ResetPasswordController().handle
-);
+router.use(categoryRouter);
+router.use(invoiceRouter);
+router.use(userRouter);
 
-//inovoice
-router.post(
-  "/invoices",
-  isAuthenticated,
-  new CreateInovoiceController().handle
-);
 export { router };
---
+++ backend/src/routes/userRoutes.ts
@@ -0,0 +1,36 @@
+import { Router } from "express";
+
+import { isAuthenticated } from "../middlewares/isAuthenticated";
+import { AuthUserController } from "../Controllers/User/auth-user/auth-user";
+import { CreateUserController } from "../Controllers/User/create-user/create-user";
+import { GetUsersController } from "../Controllers/User/get-users/get-users";
+import { deleteUserController } from "../Controllers/User/delete-user/delete-user";
+import { UpdateUserController } from "../Controllers/User/update-user/update-user";
+import { ToForgotPasswordController } from "../Controllers/User/to-forgot-password/to-forgot-password";
+import { ToRecoverPasswordController } from "../Controllers/User/to-recover-user/to-recover-password";
+import { ResetPasswordController } from "../Controllers/User/reset-password/reset-password";
+
+const userRouter = Router();
+
+userRouter.post("/session", new AuthUserController().handle);
+userRouter.post("/user", isAuthenticated, new CreateUserController().handle);
+userRouter.get("/users", isAuthenticated, new GetUsersController().handle);
+userRouter.delete(
+  "/user/:id",
+  isAuthenticated,
+  new deleteUserController().handle
+);
+userRouter.patch(
+  "/user/:id",
+  isAuthenticated,
+  new UpdateUserController().handle
+);
+userRouter.post("/forgotpassword", new ToForgotPasswordController().handle);
+userRouter.post("/recoverpassword", new ToRecoverPasswordController().handle);
+userRouter.post(
+  "/resetpassword",
+  isAuthenticated,
+  new ResetPasswordController().handle
+);
+
+export { userRouter };
