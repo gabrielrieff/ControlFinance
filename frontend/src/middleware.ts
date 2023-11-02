fd8798bf9cc609e49fcf0bd861e4c7fb17387986
Gabriel Rieff <gabrielrieff1@gmail.com>
2023-11-02 18:57:18 -0300

    refactor: improvements middleware

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,8 +1,12 @@
-import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
   const token = request.cookies.get('@nextauth.token');
+
+  if (request.nextUrl.pathname === '/' && token?.value) {
+    return NextResponse.redirect(new URL('/dashboard', request.url));
+  }
 
   if (!token?.value) {
     return NextResponse.redirect(new URL('/', request.url));
@@ -11,5 +15,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: '/dashboard/:path*'
+  matcher: ['/dashboard/:path*', '/']
 };
