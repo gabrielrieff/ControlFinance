989b4199eb922fa9c453cd8b12a0322df2ed5825
Gabriel Rieff <gabrielrieff1@gmail.com>
2023-11-02 19:47:04 -0300

    refactor: adjusts in layout and header its height

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Header } from '~/components/Header';
+import { NavBar } from '~/components/NavBar';
 
 export const metadata: Metadata = {
   title: 'Dashboard'
@@ -8,9 +9,14 @@ export const metadata: Metadata = {
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html>
-      <body className="bg-blue-300">
-        <Header />
-        {children}
+      <body className="bg-blue-300 flex h-screen pt-3 pr-3 pb-3">
+        <div className="w-[300px]">
+          <NavBar />
+        </div>
+        <div className="grid grid-rows-3 col-span-1 w-full">
+          <Header />
+          {children}
+        </div>
       </body>
     </html>
   );
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Admin } from './ui/Admin';
 
 export const Header = () => {
   return (
-    <header className="flex justify-between px-6 items-center bg-white-100 rounded-[20px]">
+    <header className="h-[85px] flex justify-between px-6 items-center bg-white-100 rounded-[20px]">
       <h1 className="font-semibold text-[30px]">Dashboard</h1>
       <Admin />
     </header>
