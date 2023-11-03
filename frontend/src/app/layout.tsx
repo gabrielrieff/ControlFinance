111e8e9a32071128925701285f49cb06066e900f
Gabriel Rieff <gabrielrieff1@gmail.com>
2023-11-03 10:26:37 -0300

    refactor: refactor font

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,12 +1,6 @@
 import type { Metadata } from 'next';
-import { Poppins } from 'next/font/google';
 import { AuthProvider } from '~/context/auth/authContext';
 import './globals.css';
-
-const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
-});
 
 export const metadata: Metadata = {
   title: 'Login'
@@ -19,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
-      <body className={poppins.className}>
+      <body>
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
