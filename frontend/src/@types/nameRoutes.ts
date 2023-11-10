ce2a3135401e37a97591c50984be03edd538790a
Gabriel Rieff <gabrielrieff1@gmail.com>
2023-11-10 11:37:53 -0300

    refactor: route title improvements

---
+++ frontend/src/@types/nameRoutes.ts
@@ -0,0 +1,9 @@
+type RouteTitles = {
+  [key: string]: string;
+};
+
+export const nameRoutes: RouteTitles = {
+  '/main/dashboard': 'Dashboard',
+  '/main/addUser': 'Adicionar novo usuário',
+  '/main/configuracao': 'Configuração de usuário'
+};
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,27 +1,32 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
+import { nameRoutes } from '~/@types/nameRoutes';
 import { Admin } from './ui/Admin';
 
 export const Header = () => {
-  const pathname = usePathname();
+  const pathname = usePathname() as string;
 
-  const handleTitle = (title: string) => {
-    const str = title.replace('/main/', '');
-    const path = str.charAt(0).toUpperCase() + str.slice(1);
+  const pageTitle = nameRoutes[pathname] || 'Título Padrão';
 
-    if (path === 'AddUser') {
-      return 'Adicionar novo usuário';
-    } else {
-      return path;
-    }
-  };
+  console.log(pageTitle);
 
-  const title = handleTitle(pathname);
+  // const handleTitle = (title: string) => {
+  //   const str = title.replace('/main/', '');
+  //   const path = str.charAt(0).toUpperCase() + str.slice(1);
+
+  //   if (path === 'AddUser') {
+  //     return 'Adicionar novo usuário';
+  //   } else {
+  //     return path;
+  //   }
+  // };
+
+  // const title = handleTitle(pathname);
 
   return (
     <header className="h-[85px] flex justify-between px-6 items-center bg-white-100 rounded-[20px]">
-      <h1 className="font-semibold text-[30px]">{title}</h1>
+      <h1 className="font-semibold text-[30px]">{pageTitle}</h1>
       <Admin />
     </header>
   );
