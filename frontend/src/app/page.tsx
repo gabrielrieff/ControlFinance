f795506872d39fa05b17fa73643dd2988295f2e2
Gabriel Rieff <gabrielrieff1@gmail.com>
2024-02-12 09:13:07 -0300

    fead: add react rook forms and zod

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,30 +2,49 @@
 import { AuthContext } from '~/context/auth/authContext';
 
 import Link from 'next/link';
-import { FormEvent, useContext, useRef, useState } from 'react';
+import { useContext, useRef, useState } from 'react';
+
+import {z} from 'zod';
+import {useForm} from 'react-hook-form';
+import {zodResolver} from '@hookform/resolvers/zod'
+
 import { AiFillEye } from 'react-icons/ai';
 import { BiSolidLock } from 'react-icons/bi';
 import { MdEmail } from 'react-icons/md';
-import { Button } from '~/components/shared/Button';
-import { Input } from '~/components/shared/Input';
+import { Input } from '~/components/shadcn/input';
+import { Button } from '~/components/shadcn/button';
+
+
+const schema = z.object({
+  email: z.string().min(1, {message: 'Por gentileza forneça um email!'}).email(),
+  password: z.string().min(1, {message: 'Por gentileza forneça a senha'})
+})
 
 export default function Home() {
   const { signIn } = useContext(AuthContext);
+  const { handleSubmit, register, formState: {errors} } = useForm({
+    mode: 'all',
+    criteriaMode: 'all',
+    resolver: zodResolver(schema),
+    defaultValues: {
+      email: '',
+      password: ''
+    }
+  })
   const emailRef = useRef<HTMLInputElement | null>(null);
   const passwordRef = useRef<HTMLInputElement | null>(null);
 
   const [viewPassword, setViewPassword] = useState(true);
 
-  async function handleLogin(event: FormEvent) {
-    event.preventDefault();
-
-    if (emailRef.current?.value != '' && passwordRef.current?.value != '') {
-      const data = {
-        email: emailRef.current?.value!,
-        password: passwordRef.current?.value!
-      };
-      await signIn(data);
-    }
+  async function handleLogin(data: any) {
+    console.log(data)
+    // if (emailRef.current?.value != '' && passwordRef.current?.value != '') {
+    //   const data = {
+    //     email: emailRef.current?.value!,
+    //     password: passwordRef.current?.value!
+    //   };
+    //   await signIn(data);
+    // }
   }
 
   function handleViewPassword() {
@@ -36,7 +55,7 @@ export default function Home() {
     <main className="flex justify-center items-center">
       <form
         autoComplete="off"
-        onSubmit={handleLogin}
+        onSubmit={() => handleLogin(handleSubmit)}
         className="w-[40%] lg:w-[60%] md:w-[75%] flex flex-col items-center justify-center gap-16 h-screen"
       >
         <h1 className="font-semibold text-[48px]">Login</h1>
@@ -46,35 +65,39 @@ export default function Home() {
           <div className="relative">
             <MdEmail size={30} className="absolute bottom-[10px] left-2" />
             <Input
+              {...register('email')}
               type="email"
-              inputref={emailRef}
-              className="focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
-              outline-green-500 bg-grey-400 ps-11 h-[50px]"
+              className="bg-grey-400 ps-11 h-[50px] bg-slate-100"
             />
           </div>
+          {errors.email && (
+            <span>{errors.email.message}</span>
+          )}
         </label>
 
         <label className="w-full">
           <span>Senha:</span>
           <div className="relative">
-            <BiSolidLock size={30} className="absolute bottom-[10px] left-2" />
+            <BiSolidLock size={30} className="absolute bottom-[35px] left-2" />
             <Input
-              inputref={passwordRef}
+              {...register('password')}
               type={viewPassword ? 'password' : 'text'}
-              className="focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
-              outline-green-500 bg-grey-400 ps-11 pe-11 h-[50px]"
+              className="ps-11 pe-11 h-[50px] bg-slate-100"
             />
             <button onClick={handleViewPassword}>
               <AiFillEye
                 size={30}
-                className="absolute bottom-[10px] right-2 hover:text-grey-600 transition-[0.3s]"
+                className="absolute bottom-[35px] right-2 hover:text-grey-600 transition-[0.3s]"
               />
             </button>
           </div>
+          {errors.password && (
+            <span>{errors.password.message}</span>
+          )}
         </label>
         <Button
           type="submit"
-          className="bg-green-500 text-[26px] font-semibold w-[40%] text-white-100 hover:bg-green-400 hover:text-grey-700 transition-[.3s]"
+          variant={"default"}
         >
           Entrar
         </Button>
