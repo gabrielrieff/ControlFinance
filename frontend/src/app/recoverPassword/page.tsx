e796367ed6060de92f255493037ba7a82fb15260
Gabriel Rieff <gabrielrieff1@gmail.com>
2024-02-13 09:11:47 -0300

    rector: improvement in the validation of the password recovery form

--- frontend/src/app/recoverPassword/page.tsx
+++ frontend/src/app/recoverPassword/page.tsx
@@ -2,26 +2,49 @@
 
 import { MdEmail } from 'react-icons/md';
 import Link from 'next/link';
-import { FormEvent, useContext, useRef } from 'react';
+import { useContext } from 'react';
 import { AuthContext } from '~/context/auth/authContext';
 import { Input } from '~/components/shadcn/input';
 import { Button } from '~/components/shadcn/button';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+
+const schema = z.object({
+  email: z
+    .string()
+    .min(1, { message: 'Por gentileza informe um email' })
+    .email({ message: 'Por gentileza informe um email valido' })
+});
+
+type formDataProps = z.infer<typeof schema>;
 
 export default function recoverPassword() {
   const { recoverPassword } = useContext(AuthContext);
-  const emailRef = useRef<HTMLInputElement | null>(null);
+  const {
+    handleSubmit,
+    register,
+    formState: { errors }
+  } = useForm<formDataProps>({
+    mode: 'all',
+    criteriaMode: 'all',
+    resolver: zodResolver(schema),
+    defaultValues: {
+      email: ''
+    }
+  });
 
-  async function handleRecoverPassword(event: FormEvent) {
-    event.preventDefault();
+  async function handleRecoverPassword(data: formDataProps) {
+    const { email } = data;
 
-    if (emailRef.current?.value != '') {
-      await recoverPassword(emailRef.current?.value!);
+    if (email != '') {
+      await recoverPassword(email);
     }
   }
   return (
     <main className="flex justify-center items-center">
       <form
-        onSubmit={handleRecoverPassword}
+        onSubmit={handleSubmit(handleRecoverPassword)}
         className="w-[40%] lg:w-[60%] md:w-[75%] flex flex-col items-center justify-center gap-16 h-screen"
       >
         <h1 className="font-semibold text-[48px]">Recuperar senha</h1>
@@ -31,16 +54,18 @@ export default function recoverPassword() {
           <div className="relative">
             <MdEmail size={30} className="absolute bottom-[10px] left-2" />
             <Input
+              {...register('email')}
               type="email"
-              ref={emailRef}
               className="bg-slate-100 ps-11 h-[50px]"
             />
           </div>
+          {errors.email && (
+            <span className="text-red-700 font-semibold">
+              {errors.email.message}
+            </span>
+          )}
         </label>
-        <Button
-          type="submit"
-          variant={'default'}
-        >
+        <Button type="submit" variant={'default'}>
           Recuperar
         </Button>
 
